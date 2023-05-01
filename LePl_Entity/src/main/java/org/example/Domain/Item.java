9c99bdce6831e5de1c47e889a1589972d032fe71
yujinchoi20 <dalsu4641@naver.com>
2023-05-01 15:34:42 +0900

    add: LePl Entity

---
+++ LePl_Entity/src/main/java/org/example/Domain/Item.java
@@ -0,0 +1,29 @@
+package org.example.Domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private Boolean wearing_status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getWearing_status() {
+        return wearing_status;
+    }
+
+    public void setWearing_status(Boolean wearing_status) {
+        this.wearing_status = wearing_status;
+    }
+}
