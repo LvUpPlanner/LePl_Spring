95ff4cc64fe5c6ebe48f376a42c535603cafd916
BH946 <kobh2895@gmail.com>
2023-05-04 01:00:15 +0900

    add : 브랜치 생성 및 프로젝트 초기세팅 완료

--- LePl_Entity/src/main/java/org/example/Domain/Timer.java
+++
@@ -1,4 +0,0 @@
-package org.example.Domain;
-
-public class Timer {
-}
---
+++ lvpl/src/main/java/com/lvpl/LvplApplication.java
@@ -0,0 +1,13 @@
+package com.lvpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvplApplication.class, args);
+	}
+
+}
