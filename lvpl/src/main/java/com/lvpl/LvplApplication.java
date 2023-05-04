6cb6c732193559d8db4fba05e5676d8818a96316
BH946 <kobh2895@gmail.com>
2023-05-04 22:30:12 +0900

    add : 회원 레퍼지토리, 서비스 TDD 구현

--- lvpl/src/main/java/com/lvpl/LvplApplication.java
+++ lvpl/src/main/java/com/lvpl/LvplApplication.java
@@ -1,17 +1,18 @@
 package com.lvpl;
 
+import com.lvpl.Repository.MemberRepository;
+import com.lvpl.Service.MemberService;
 import com.lvpl.domain.Lists;
 import com.lvpl.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LvplApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LvplApplication.class, args);
-
 	}
 
 }
--- lvpl/src/main/java/com/lvpl/Repository/MemberRepository.java
+++ lvpl/src/main/java/com/lvpl/Repository/MemberRepository.java
@@ -1,2 +1,28 @@
-package com.lvpl.Repository;public class MemberRepository {
+package com.lvpl.Repository;
+
+import com.lvpl.domain.Member;
+import com.sun.istack.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor // 생성자 주입 + 엔티티매니저 주입 제공
+public class MemberRepository {
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member); // db 저장
+    }
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+    public Member findByUid(String uid) {
+        List<Member> findMembers = em.createQuery("select m from Member m where m.uid = :uid", Member.class)
+                .setParameter("uid", uid)
+                .getResultList(); // List로 반환 받아야 null처리가 쉬움
+        return findMembers.isEmpty() ? null : findMembers.get(0);
+    }
 }
