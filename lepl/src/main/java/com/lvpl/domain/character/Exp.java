8061287cdf6625471cd593a931618c0e2d59704a
BH946 <kobh2895@gmail.com>
2023-09-12 15:21:54 +0900

    add : 프로젝트 버전 업그레이드

---
+++ lepl/src/main/java/com/lvpl/domain/character/Exp.java
@@ -0,0 +1,20 @@
+package com.lvpl.domain.character;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Exp {
+    @Id @GeneratedValue
+    @Column(name = "exp_id")
+    private Long id;
+
+    private Long expAll;
+}
