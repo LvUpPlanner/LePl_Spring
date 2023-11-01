025ecee82ba65c453f310050044a61b386694412
BH946 <kobh2895@gmail.com>
2023-11-01 15:02:00 +0900

    test: Member 도메인 테스트코드 작성

---
+++ lepl/src/test/java/com/lepl/domain/member/MemberTest.java
@@ -0,0 +1,29 @@
+package com.lepl.domain.member;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberTest {
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    public void 중복회원_예외() throws Exception {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+}
